af1d23af7cf434c5a43a84dad57c24f9687f6ceb
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-02-09 21:55:19 -0300

    :sparkles: feat: enhance CrawlerController to include crawl endpoint with URL query handling

--- api/src/modules/crawler/crawler.controller.ts
+++ api/src/modules/crawler/crawler.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { CrawlerService } from './crawler.service';
 
 @Controller('crawler')
-export class CrawlerController {}
+export class CrawlerController {
+  constructor(private readonly crawlerService: CrawlerService) {}
+
+  @Get()
+  async crawl(@Query('url') url: string) {
+    if (!url) {
+      return 'Please provide a URL to crawl';
+    }
+
+    const result = await this.crawlerService.crawl(url);
+  }
+}
