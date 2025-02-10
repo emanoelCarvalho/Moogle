6e436eb3cc04d271863f30a15a4fae96519edd6f
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-02-10 17:15:49 -0300

    :sparkles: feat: export IndexerService from IndexerModule for external usage

--- api/src/modules/indexer/indexer.module.ts
+++ api/src/modules/indexer/indexer.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { IndexerService } from './indexer.service';
 
 @Module({
+  exports: [IndexerService],
   providers: [IndexerService]
 })
 export class IndexerModule {}
