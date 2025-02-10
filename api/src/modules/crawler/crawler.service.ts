474f4a0e751b6344d596f7a0b6d4588f413f0456
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-02-09 21:53:20 -0300

    :sparkles: feat: implement CrawlerService with web crawling functionality using axios and cheerio

--- api/src/modules/crawler/crawler.service.ts
+++ api/src/modules/crawler/crawler.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+import * as cheerio from 'cheerio';
 
 @Injectable()
-export class CrawlerService {}
+export class CrawlerService {
+    async crawl(url: string) {
+        try {
+            const {data} = await axios.get(url);
+            const $ = cheerio.load(data); 
+
+            const pageTitle = $('title').text();
+            let links: string[] = [];
+
+            $('a').each((_, element) => {
+                const href = $(element).attr('href');
+
+                if (href && href.startsWith('http')) {
+                    links = [...links, href];
+                }
+            }); 
+
+            return {
+                url, 
+                title: pageTitle,
+                links
+            }; 
+        } catch (error) {
+            console.error(`Error crawling ${url}: `, error.message);
+            return null;
+        }
+    }
+}
