74a8c470319f3767c6940ba5e88c9239ae4e5a3a
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-02-10 16:59:28 -0300

    :sparkles: feat: integrate TreeService into CrawlerService for enhanced URL indexing and searching

--- api/src/modules/crawler/crawler.service.ts
+++ api/src/modules/crawler/crawler.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 import * as cheerio from 'cheerio';
+import { TreeService } from '../tree/tree.service';
 
 @Injectable()
 export class CrawlerService {
+  constructor(private readonly treeService: TreeService) {}
+
   async crawl(url: string) {
     try {
       const { data } = await axios.get(url, {
-        headers: { 'User-Agent': 'Mozilla/5.0' } 
+        headers: { 'User-Agent': 'Mozilla/5.0' },
       });
 
       const $ = cheerio.load(data);
@@ -15,18 +18,28 @@ export class CrawlerService {
       const title = $('title').text();
 
       let links: string[] = [];
-      
+
       $('a').each((_, element) => {
         const href = $(element).attr('href');
         if (href && href.startsWith('http')) {
           links = [...links, href];
         }
-    });
-    
-    return { url, title, links };
+      });
+
+      this.treeService.insert(url, title, links);
+
+      return { url, title, links };
     } catch (error) {
       console.error('Erro ao fazer crawling:', error.message);
       return { error: 'Erro ao processar a URL. Verifique se está acessível.' };
     }
+  }
+
+  getAllIndexedPages() {
+    return this.treeService.getAll();
+  }
+
+  searchByTitle(title: string) {
+    return this.treeService.search(title);
   }
 }
