ed7db13bd4556ae21822a1cacd8f133eb62f3ab0
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-02-10 17:16:11 -0300

    :sparkles: feat: add IndexerModule to CrawlerModule imports for enhanced functionality

--- api/src/modules/crawler/crawler.module.ts
+++ api/src/modules/crawler/crawler.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CrawlerController } from './crawler.controller';
 import { CrawlerService } from './crawler.service';
 import { TreeModule } from '../tree/tree.module';
+import { IndexerModule } from '../indexer/indexer.module';
 
 @Module({
-  imports: [TreeModule],
+  imports: [TreeModule, IndexerModule],
   controllers: [CrawlerController], 
   providers: [CrawlerService],
   exports: [CrawlerService]
