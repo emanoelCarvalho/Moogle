bedbe1215d58f09dcf99453cb9bbc1fde5809c78
Gean de Lima <117093951+Foccuns169@users.noreply.github.com>
2025-02-23 21:24:42 -0300

    Telas

---
+++ client-moogle/src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container class="d-flex flex-column align-center justify-center fill-height">
+    <v-card class="pa-6 text-center" elevation="10" color="blue darken-3">
+      <v-card-title class="text-h4 white--text">NexSearch</v-card-title>
+      <v-text-field
+        v-model="searchQuery"
+        label="Digite sua busca..."
+        outlined
+        dense
+        class="mt-4"
+        color="blue lighten-3"
+      ></v-text-field>
+      <v-btn color="blue darken-1" class="mt-4 white--text" @click="search">Buscar</v-btn>
+      <v-btn color="blue lighten-2" class="mt-4 ml-2 white--text" @click="lucky">Estou com sorte</v-btn>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchQuery: ''
+    };
+  },
+  methods: {
+    search() {
+      this.$router.push({ name: 'results', query: { q: this.searchQuery } });
+    },
+    lucky() {
+      alert('Funcionalidade "Estou com sorte" ainda não implementada!');
+    }
+  }
+};
+</script>
---
+++ client-moogle/src/views/Results.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container>
+    <v-card class="pa-6" color="blue darken-3">
+      <v-card-title class="text-h5 white--text">Resultados da Pesquisa</v-card-title>
+      <v-list>
+        <v-list-item v-for="(site, index) in results" :key="index" @click="preview(site)" class="blue lighten-4">
+          <v-list-item-content>
+            <v-list-item-title>{{ site.title }}</v-list-item-title>
+            <v-list-item-subtitle>{{ site.description }}</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <div class="tree-visual text-center mt-4">🌳 Árvore de busca...</div>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      results: [
+        { title: 'Site 1', description: 'Descrição do site 1...' },
+        { title: 'Site 2', description: 'Descrição do site 2...' }
+      ]
+    };
+  },
+  methods: {
+    preview(site) {
+      this.$router.push({ path: '/preview', query: { site: site.title } });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.tree-visual {
+  font-size: 24px;
+  margin-top: 20px;
+  background: rgba(0, 0, 0, 0.2);
+  padding: 15px;
+  border-radius: 8px;
+  color: white;
+}
+</style>
