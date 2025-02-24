96d433a3bebb41d6e3fd38cd489554848816ee38
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-02-23 23:26:19 -0300

    :sparkles: feat: implement loadData method in DataLoaderService for loading data from db.json

--- api-moogle/src/data-loader/data-loader.service.ts
+++ api-moogle/src/data-loader/data-loader.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import * as fs from 'fs';
 
 @Injectable()
-export class DataLoaderService {}
+export class DataLoaderService {
+  private readonly filePath = './db.json';
+
+  loadData(): any {
+    try {
+      const rawData = fs.readFileSync(this.filePath, 'utf8');
+      return JSON.parse(rawData);
+    } catch (error) {
+      console.error('Erro ao carregar db.json:', error);
+      return { pages: [], index: [] };
+    }
+  }
+}
